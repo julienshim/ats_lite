ac0aced3a777ce4eb84497c4d25c7389e69d15d7
julienshim <25723428+julienshim@users.noreply.github.com>
2023-02-26 21:14:49 -0800

    pji

--- fetch.py
+++ fetch.py
@@ -10,3 +10,14 @@ def get_soup(page_number):
     response = get('')
     soup = BeautifulSoup(response.text, 'html.parser')
     return soup
+
+def parse_job_item(job_item):
+    position = job_item.find('a', {'id': compile(r'jotTitle_PIPE-[0-9]{1,}')})
+    job_title = position.text
+    job_href = position["href"]
+    job_link = ''
+    job_reference = job_href.split('/')[3]
+    department = job_item.find('span', {'class': compile(r'table--advanced-search__role')}).text
+    date = job_item.find('span', {'class': compile(r'table--advanced-search__date')}).text
+    [month, day, year] = split(r'[ ,]{1,}', date)
+    location = job_item.find('span', {'id': compile(r'storeName_container_PIPE-[0-9]{1,}')}).text
