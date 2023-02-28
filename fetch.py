d06341a4a0fac17cceb4db0e4f1818af5e4cf69c
julienshim <25723428+julienshim@users.noreply.github.com>
2023-02-27 21:15:25 -0800

    pji2

--- fetch.py
+++ fetch.py
@@ -21,3 +21,29 @@ def parse_job_item(job_item):
     date = job_item.find('span', {'class': compile(r'table--advanced-search__date')}).text
     [month, day, year] = split(r'[ ,]{1,}', date)
     location = job_item.find('span', {'id': compile(r'storeName_container_PIPE-[0-9]{1,}')}).text
+
+    dates_dict = {
+        'Jan': '01',
+        'Feb': '02',
+        'Mar': '03',
+        'Apr': '04',
+        'May': '05',
+        'Jun': '06',
+        'Jul': '07',
+        'Aug': '08',
+        'Sep': '09',
+        'Oct': '10',
+        'Nov': '11',
+        'Dec': '12'
+    }
+
+    job_items_dict = {
+        'import_date': f'{year}-{dates_dict[month]}-{day}',
+        'job_title': job_title,
+        'job_link': job_link,
+        'job_reference': job_reference,
+        'department': department,
+        'location': location
+    }
+    
+    return job_items_dict
