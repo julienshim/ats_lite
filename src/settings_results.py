eff4c1aae299e0c51137c61f8553d03178eb22e3
julienshim <25723428+julienshim@users.noreply.github.com>
2023-03-26 21:02:32 -0700

    init

---
+++ src/settings_results.py
@@ -0,0 +1,16 @@
+from random import randint
+
+csv_output_path = ''
+
+seconds_range_between_fetch = randint(15, 30)
+
+search_parameters = {
+    'location': ['']
+}
+
+no_of_weeks_limit = 2
+
+ignore = {
+    "department": [''],
+    "location": ['']
+}
