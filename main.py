83ca542ccf15ca84a3907ce1b9e834e59337179d
julienshim <25723428+julienshim@users.noreply.github.com>
2023-04-01 21:08:59 -0700

    rojlpij

--- main.py
+++ main.py
@@ -13,3 +13,16 @@ from bs4 import BeautifulSoup
 from src.settings_results import ignore, csv_output_path, seconds_range_between_fetch
 from src.functions import get_job_posting_details, get_target_locations, get_date_n_weeks_ago
 
+def remove_old_jobs(job):
+    [y, m, d] = list(map(lambda x: int(x), job[0].split('-')))
+    jid = datetime(y, m, d)
+    cod = get_date_n_weeks_ago()
+    return jid >= cod
+
+def load_previously_imported_jobs():
+    with open(csv_output_path) as ref:
+        data = [row for row in reader(ref)][1:]
+        data = sorted(data, key=lambda x: x[0], reverse=True)
+        data = list(filter(remove_old_jobs, data))
+        # data = list(map(get_job_ref_date, data))
+        return data
